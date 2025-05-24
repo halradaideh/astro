8643a13de43ec8aca56d9690f0ddfb8b6b7df36b
Hamdan Al-Radaideh <hradaideh@atypon.com>
2025-05-24 19:15:37 +0300

    fix format

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,7 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import type { CollectionEntry } from 'astro:content';
 
 export async function GET(context: { site: URL }) {
   const posts = await getCollection('blog');
@@ -8,7 +9,7 @@ export async function GET(context: { site: URL }) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
+    items: posts.map((post: CollectionEntry<'blog'>) => ({
       ...post.data,
       link: `/blog/${post.id}/`,
     })),
